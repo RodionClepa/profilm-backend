74fe3fdbbaf1291fa94a130552a3943d3569e331
RodionClepa <raduclepa@gmail.com>
2025-03-29 14:33:54 +0200

    feat(orm): add sync database in main file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import sequelize from './database/init.js';
+import testRouter from './routes/test.routes.js';
 
 const app: Express = express();
 const port = 3000;
@@ -7,12 +8,20 @@ const port = 3000;
 try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    
+    await sequelize.sync({ force: false });
+    console.log('Database synced successfully!');
+
+    console.log(sequelize.models);
+
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
 
+app.use("/api/movies", testRouter);
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!11!=');
+    res.send('Hello World!=');
 });
 
 app.listen(port, () => {
