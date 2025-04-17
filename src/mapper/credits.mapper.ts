68285b23446282a38f34bf9961d6bccf2b30cdca
RodionClepa <raduclepa@gmail.com>
2025-04-17 19:35:06 +0300

    fix(cast): change id field

--- src/mapper/credits.mapper.ts
+++ src/mapper/credits.mapper.ts
@@ -4,7 +4,7 @@ import { CastResponse, RawCast } from "../types/credits.type.js";
 export const mapCast = (rawMovieCast: RawCast): CastResponse => ({
   adult: rawMovieCast.adult,
   gender: rawMovieCast.gender,
-  id: rawMovieCast.cast_id,
+  id: rawMovieCast.id,
   name: rawMovieCast.name,
   character: rawMovieCast.character,
   profilePath: rawMovieCast.profile_path ? config.FILM_IMAGE_API_URL + PROFILE_SIZES.medium + rawMovieCast.profile_path : null,
